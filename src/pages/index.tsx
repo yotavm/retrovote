5a96406e3415e96fad362a4c7b6d792658611fda
iamyotav <masayotav@gmail.com>
2023-03-29 01:44:09 +0300

    add input design and update prism schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,16 +12,29 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Retro<span className="text-[#0098eb]">Vote</span>
           </h1>
           {!isLoaded && <p>Loading...</p>}
           {!isSignedIn && <SignInButton />}
-          {isSignedIn && <p>Logged in!</p>}
+          {isSignedIn && <PollCreation />}
         </div>
       </main>
     </>
+  );
+};
+
+const PollCreation = () => {
+  return (
+    <div className="flex w-full gap-4">
+      <input
+        className="flex-1 rounded-md border-2 bg-transparent px-2 py-2 text-white outline-none"
+        type="text"
+        placeholder="Poll Title"
+      />
+      <button>Create</button>
+    </div>
   );
 };
 
