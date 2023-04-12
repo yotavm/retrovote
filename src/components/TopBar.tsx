565dde2c4d9c2d2f253f128483d44176715eeea5
iamyotav <masayotav@gmail.com>
2023-04-12 19:19:46 +0300

    add back link to logo

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export const TopBar = () => {
   return (
     <div className="w-full border-b border-b-slate-700 bg-slate-900">
       <div className="flex h-16 items-center p-6">
         <div className="text-xl font-extrabold tracking-tight text-white">
-          Retro<span className="text-[#0098eb]">Vote</span>
+          <Link href={"/"}>
+            Retro<span className="text-[#0098eb]">Vote</span>
+          </Link>
         </div>
       </div>
     </div>
