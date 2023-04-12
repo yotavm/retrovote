e607865bee9d3515440ec8cc8a62b2eccab1c90e
iamyotav <masayotav@gmail.com>
2023-04-12 17:45:25 +0300

    add react-hot-toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Toaster />
       <Component {...pageProps} />
     </ClerkProvider>
   );
