48aab63f49227853f56252ad8adfae7e830da5a2
iamyotav <masayotav@gmail.com>
2023-05-11 02:06:25 +0300

    fix anyanomsID

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,11 +1,12 @@
 export const getAnyanomesId = () => {
   const cookieName = "anyanomesId";
   const cookieString = document.cookie;
-  const cookies = cookieString
-    .split(";")
+  const cookieValue = cookieString
+    .split(" ")
     .find((cookie) => {
       return cookie.startsWith(cookieName);
     })
     ?.split("=")[1];
-  return cookies as string;
+  console.log("cookieValue", cookieValue);
+  return cookieValue as string;
 };
